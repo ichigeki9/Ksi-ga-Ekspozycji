5a69b104e6565ee6bf065c768035b0493e1c5dbb
Patryk <p.karpinski95@wp.pl>
2026-01-20 13:06:16 +0100

    Poprawiono UX/UI i przeniesiono .gitignore do roota

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,19 @@
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, FileText, TriangleAlert } from "lucide-react";
 import NewItemForm from "./components/NewItemForm";
 import { useState } from "react";
 
 const exp = [
 	{
 		lp: 1,
+		date: "10-10-2025",
+		personelName: "Patryk Karpiński",
+		patientName: "Mirek Kowalski",
+		medicalReport: "qqqq",
+		eventDescription: "wwww",
+		actionsAfterExposure: "qwqwqw",
+	},
+	{
+		lp: 2,
 		date: "10-10-2025",
 		personelName: "Patryk Karpiński",
 		patientName: "Mirek Kowalski",
@@ -32,91 +41,108 @@ function App() {
 			{visibleForm && (
 				<NewItemForm setVisibleForm={setVisibleForm} setData={setData} />
 			)}
-			<div className="w-full h-screen  flex justify-start items-center flex-col p-6 ">
-				<h1 className="text-black text-4xl font-semibold mb-10">
-					KSIĘGA EKSPOZYCJI ZAWODOWYCH
-				</h1>
-				<div className="border-2 bg-gray-400 w-full mt-10 text-white rounded-2xl p-4 mb-4">
-					<h2 className="text-2xl font-semibold mb-4">
-						INSTRUKCJA POSTĘPOWANIA W RAZIE EKSPOZYCJI
-					</h2>
-					<p className="mb-2 font-semibold">
-						1. W RAZIE EKSPOZYCJI DZWONIĆ NA KARETKĘ
-					</p>
-					<p className="mb-2 font-semibold">
-						2. Wypełnić formularz dostępny poniżej
-					</p>
-					<p className="mb-2 font-semibold">
-						3. Wydrukowac oraz wypełnić wniosek nr 1, 2 ,3{" "}
-					</p>
-				</div>
+			<div className="w-full min-h-screen flex justify-start items-center flex-col p-6 bg-gray-50">
+			<div className="w-full max-w-[1200px] flex flex-col items-center">
+			
+					<h1 className="text-blue-600 text-5xl font-bold mb-10">
+						KSIĘGA EKSPOZYCJI ZAWODOWYCH
+					</h1>
+					{/* <div>div</div> */}
+					<div className=" bg-amber-200 w-full mt-10 mb-8 rounded-2xl p-4 shadow-md">
+						<div className="flex w-full items-start justify-start">
+							<h2 className="text-2xl font-semibold mb-4 mr-4">
+								INSTRUKCJA POSTĘPOWANIA W RAZIE EKSPOZYCJI
+							</h2>
+							<TriangleAlert width={30} height={30} color="#8C1627" />
+						</div>
+						<p className="mb-2 font-semibold text-xl">
+							1. W razie ekspozycji zgłosić bezpośredniemu przełożonemu.
+						</p>
+						<p className="mb-2 font-semibold text-xl">
+							2. Wypełnić, wydrukować i przekazać dla bezpośredniego
+							przełożonego
+							<span className="underline font-semibold"> formularz nr 1</span>
+						</p>
+						<p className="mb-2 font-semibold text-xl">
+							3. Jeśli wymagana konsultacja w Białymstoku wypełnić{" "}
+							<span className="underline ">formularz nr. 2 i nr. 3.</span>
+						</p>
+					</div>
 
-				<button
-					onClick={() => setVisibleForm(true)}
-					className="w-full border-2 rounded-2xl p-4 cursor-pointer bg-blue-300 hover:bg-blue-500 flex items-center justify-center gap-4 font-semibold text-xl">
-					DODAJ EKSPOZYCJE <CirclePlus />
-				</button>
-				<div className="w-full  mt-5">
 					<button
-						onClick={() => alert("Drukuje wniosek 1")}
-						className="cursor-pointer w-full border-2 py-2 rounded-2xl bg-gray-400 hover:bg-blue-500 text-xl  mb-2">
-						{" "}
-						Wniosek 1
+						onClick={() => setVisibleForm(true)}
+						className="w-full rounded-2xl p-4 py-6 text-white cursor-pointer bg-blue-600 hover:bg-blue-400 flex items-center justify-center gap-4 font-semibold text-2xl shadow-xl mb-4">
+						DODAJ EKSPOZYCJE <CirclePlus />
 					</button>
-					<button
-						onClick={() => alert("Drukuje wniosek 2")}
-						className="cursor-pointer w-full border-2 py-2 rounded-2xl bg-gray-400 hover:bg-blue-500 text-xl  mb-2">
-						{" "}
-						Wniosek 2
-					</button>
-					<button
-						onClick={() => alert("Drukuje wniosek 3")}
-						className="cursor-pointer w-full border-2 py-2 rounded-2xl bg-gray-400 hover:bg-blue-500 text-xl  mb-2">
-						{" "}
-						Wniosek 3
-					</button>
-				</div>
-				<div className="w-full h-full flex items-start justify-start  border-2 mt-10 p-4">
-					<ul className=" flex items-start justify-start flex-col ">
-						<li className="w-full flex items-center justify-start gap-15 text-2xl border-b mb-2">
-							<div className="w-full ">
-								<p>Lp.</p>
-							</div>
-							<div className="w-full ">
-								<p>Data Ekspozycji</p>
-							</div>
-							<div className="w-full ">
-								<p>Imię i nazwisko pracownika</p>
-							</div>
-							<div className="w-full ">
-								<p>Imie i nazwisko pacjenta</p>
-							</div>
-						</li>
-						{data.length > 0 ? (
-							data.map((item) => (
-								<li
-									key={item.id}
-									className="w-full flex items-center justify-start gap-15 text-2xl mb-2 ">
+					<div className="w-full flex gap-10 mt-5">
+						<button
+							onClick={() => alert("Drukuje wniosek 1")}
+							className="cursor-pointer w-full border-2 p-4 rounded-2xl hover:text-white hover:bg-blue-600 text-xl border-blue-600 text-blue-600 mb-4 font-semibold flex items-center justify-center gap-5 ">
+							{" "}
+							Formularz nr. 1
+							<FileText />
+						</button>
+						<button
+							onClick={() => alert("Drukuje wniosek 2")}
+							className="cursor-pointer w-full border-2 p-4 rounded-2xl hover:text-white hover:bg-blue-600 text-xl border-blue-600 text-blue-600 mb-4 font-semibold flex items-center justify-center gap-5 ">
+							{" "}
+							Formularz nr. 2
+							<FileText />
+						</button>
+						<button
+							onClick={() => alert("Drukuje wniosek 3")}
+							className="cursor-pointer w-full border-2 p-4 rounded-2xl hover:text-white hover:bg-blue-600 text-xl border-blue-600 text-blue-600 mb-4 font-semibold flex items-center justify-center gap-5 ">
+							{" "}
+							Formularz nr. 3
+							<FileText />
+						</button>
+					</div>
+
+					<div className="bg-gray-100 border w-full h-full flex flex-col items-center justify-center rounded-2xl shadow-2xl p-2 mt-6">
+						<h2 className="mt-10 text-4xl font-semibold">LISTA EKSPOZYCJI </h2>
+						<div className="w-full h-full flex items-start justify-start mt-10 p-4 rounded-2xl ">
+							<ul className=" w-full flex items-start justify-start flex-col">
+								<li className="w-full flex items-center justify-start gap-15 text-2xl border-b mb-2 p-2">
 									<div className="w-full ">
-										<p>{item.id}.</p>
+										<p>Lp.</p>
 									</div>
 									<div className="w-full ">
-										<p>{item.date}</p>
+										<p>Data Ekspozycji</p>
 									</div>
 									<div className="w-full ">
-										<p>{showFirstLetter(item.personelName)}</p>
+										<p>Imię i nazwisko pracownika</p>
 									</div>
 									<div className="w-full ">
-										<p>{showFirstLetter(item.patientName)}</p>
+										<p>Imie i nazwisko pacjenta</p>
 									</div>
 								</li>
-							))
-						) : (
-							<p>Brak </p>
-						)}
-					</ul>
-				</div>
+								{data.length > 0 ? (
+									data.map((item) => (
+										<li
+											key={item.lp}
+											className="w-full flex items-center justify-start gap-15 text-2xl mb-2 ">
+											<div className="w-full ">
+												<p>{item.lp}.</p>
+											</div>
+											<div className="w-full ">
+												<p>{item.date}</p>
+											</div>
+											<div className="w-full ">
+												<p>{showFirstLetter(item.personelName)}</p>
+											</div>
+											<div className="w-full ">
+												<p>{showFirstLetter(item.patientName)}</p>
+											</div>
+										</li>
+									))
+								) : (
+									<p>Brak </p>
+								)}
+							</ul>
+						</div>
+					</div>
 			</div>
+		</div>
 		</>
 	);
 }
--- frontend/src/components/NewItemForm.jsx
+++ frontend/src/components/NewItemForm.jsx
@@ -10,113 +10,220 @@ export default function NewItemForm(props) {
 
 	const onSubmit = (data) => {
 		console.log(data);
-    props.setData(prevVal => [...prevVal, data])
-     alert('Wyslano ekspozycje');
+		props.setData((prevVal) => [...prevVal, data]);
+		alert("Wyslano ekspozycje");
 	};
 
 	return (
-		<div className="w-full h-screen absolute bg-gray-500 flex items-center justify-start flex-col p-6">
-			<div className="mb-10 mt-4 flex justify-center items-center w-full ">
+		<div className="w-full  absolute bg-gray-50  flex items-center justify-start flex-col ">
+			<div className="mb-10 mt-4 flex justify-center items-center w-full  py-5">
 				<button
-					className=" absolute w-10  left-20 cursor-pointer"
+					className=" absolute w-10  left-20 cursor-pointer "
 					onClick={() => props.setVisibleForm(false)}>
-					<CircleArrowLeft color="#fff" width={35} height={35} />
+					<CircleArrowLeft color="#155dfc" width={50} height={50} />
 				</button>
-				<h1 className="text-white text-4xl font-semibold">
+				{/* <div cl> */}
+				<h1 className=" text-blue-600 text-5xl font-bold">
 					FORMULARZ EKSPOZYCJI
 				</h1>
+				{/* </div> */}
 			</div>
 
 			<form
 				onSubmit={handleSubmit(onSubmit)}
-				className="flex flex-col items-start justify-start w-full border-2 p-4 border-amber-200">
-				<div className="flex flex-col gap-2 mb-3">
-					<label>data i godzina zdarzenia</label>
-					<input
-						type="datetime-local"
-						className="border-2"
-						{...register("date")}
-					/>
+				className="flex flex-col items-start justify-start max-w-[1200px] p-8 ">
+				{/* POSZKODOWANY */}
+
+				<div className="bg-gray-100 border w-full flex flex-col justify-start items-start shadow-md rounded-2xl p-4 mb-8">
+					<h2 className="font-semibold text-2xl mb-2">Dane Poszkodowanego</h2>
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Stanowisko</label>
+						<input
+						placeholder="Np. Pielęgniarka"
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("position", {
+								required: "Pole wymagane",
+								minLength: { value: 2, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.position && (
+							<p className="text-red-500 text-sm">{errors.position.message}</p>
+						)}
+					</div>
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Oddział / Komórka Organizacyjna</label>
+						<input
+						placeholder="Np. Kardiologia"
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("ward", {
+								required: "Pole wymagane",
+								minLength: { value: 2, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.ward && (
+							<p className="text-red-500 text-sm">{errors.ward.message}</p>
+						)}
+					</div>
+
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Imie i nazwisko osoby poszkodowanej</label>
+						<input
+						placeholder="Np. Jan Nowak"
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("personelName", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.personelName && (
+							<p className="text-red-500 text-sm">
+								{errors.personelName.message}
+							</p>
+						)}
+					</div>
 				</div>
 
-				<div className="flex flex-col gap-2 mb-3">
-					<label>Imie i nazwisko osoby poszkodowanej</label>
-					<input
-						className="border-2"
-						{...register("personelName", {
-							required: "Pole wymagane",
-							minLength: { value: 3, message: "Min. 3 znaki" },
-							maxLength: { value: 20, message: "Max. 20 znaków" },
-						})}
-					/>
-					{errors.personelName && (
-						<p className="text-red-500 text-sm">{errors.personelName.message}</p>
-					)}
-				</div>
-				<div className="flex flex-col gap-2 mb-3">
-					<label>Imie i nazwisko osoby pacjenta</label>
-					<input
-						className="border-2"
-						{...register("patientName", {
-							required: "Pole wymagane",
-							minLength: { value: 3, message: "Min. 3 znaki" },
-							maxLength: { value: 20, message: "Max. 20 znaków" },
-						})}
-					/>
-					{errors.patientName && (
-						<p className="text-red-500 text-sm">{errors.patientName.message}</p>
-					)}
-				</div>
-				<div className="flex flex-col gap-2 mb-3">
-					<label>wywiad medyczny pacjenta</label>
-					<textarea
-						className="border-2"
-						{...register("medicalReport", {
-							required: "Pole wymagane",
-							minLength: { value: 3, message: "Min. 3 znaki" },
-							maxLength: { value: 20, message: "Max. 20 znaków" },
-						})}
-					/>
-					{errors.medicalReport && (
-						<p className="text-red-500 text-sm">{errors.medicalReport.message}</p>
-					)}
-				</div>
-				<div className="flex flex-col gap-2 mb-3">
-					<label>
-						dokładny opis zdarzenia , miejsce , rodzaj ekspozycji, sytuacja
-					</label>
-					<textarea
-						className="border-2"
-						{...register("eventDescription", {
-							required: "Pole wymagane",
-							minLength: { value: 3, message: "Min. 3 znaki" },
-							maxLength: { value: 20, message: "Max. 20 znaków" },
-						})}
-					/>
-					{errors.eventDescription && (
-						<p className="text-red-500 text-sm">{errors.eventDescription.message}</p>
-					)}
+				{/* DANE PACJENTA */}
+
+				<div className="bg-gray-100 border w-full flex flex-col justify-start items-start shadow-md rounded-2xl p-4 mb-8">
+					<h2 className="font-semibold text-2xl mb-2">Dane Pacjenta</h2>
+
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Imie i nazwisko osoby pacjenta</label>
+						<input
+						placeholder="Np. Jan Nowak"
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("patientName", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.patientName && (
+							<p className="text-red-500 text-sm">
+								{errors.patientName.message}
+							</p>
+						)}
+					</div>
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Wywiad medyczny pacjenta</label>
+						<textarea
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("medicalReport", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.medicalReport && (
+							<p className="text-red-500 text-sm">
+								{errors.medicalReport.message}
+							</p>
+						)}
+					</div>
 				</div>
 
-				<div className="flex flex-col gap-2 mb-3">
-					<label>opis czynności wykonanych po ekspozycji</label>
-					<textarea
-						className="border-2"
-						{...register("actionsAfterExposure", {
-							required: "Pole wymagane",
-							minLength: { value: 3, message: "Min. 3 znaki" },
-							maxLength: { value: 20, message: "Max. 20 znaków" },
-						})}
-					/>
-					{errors.actionsAfterExposure && (
-						<p className="text-red-500 text-sm">{errors.actionsAfterExposure.message}</p>
-					)}
+				{/* ZDARZENIE */}
+				<div className="bg-gray-100 border w-full flex flex-col justify-start items-start shadow-md rounded-2xl p-4 mb-8">
+					<h2 className="font-semibold text-2xl mb-2">Dane Zdarzenia</h2>
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Data i godzina zdarzenia</label>
+						<input
+							type="datetime-local"
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("date", {
+								required: "Pole wymagane",
+							})}
+						/>
+						{errors.date && (
+							<p className="text-red-500 text-sm">{errors.date.message}</p>
+						)}
+					</div>
+
+					{/* Rodzaj ekspozycji (np. zakłucie, skaleczenie, kontakt błon śluzowych, aerozol)  */}
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">
+							Rodzaj ekspozycji (np. zakłucie, skaleczenie, kontakt błon
+							śluzowych, aerozol)
+						</label>
+						<input
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("typeOfExposure", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.typeOfExposure && (
+							<p className="text-red-500 text-sm">
+								{errors.typeOfExposure.message}
+							</p>
+						)}
+					</div>
+					{/* Rodzaj materiału biologicznego (krew, płyn ustrojowy) */}
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Rodzaj materiału biologicznego (krew, płyn ustrojowy)</label>
+						<input
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("typeOfMaterial", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.typeOfMaterial && (
+							<p className="text-red-500 text-sm">
+								{errors.typeOfMaterial.message}
+							</p>
+						)}
+					</div>
+
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">
+							Dokładny opis zdarzenia , miejsce , rodzaj ekspozycji, sytuacja
+						</label>
+						<textarea
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("eventDescription", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.eventDescription && (
+							<p className="text-red-500 text-sm">
+								{errors.eventDescription.message}
+							</p>
+						)}
+					</div>
+
+					{/* 6. Znany / nieznany status źródła */}
+
+					<div className="flex flex-col gap-2 mb-3 w-full">
+						<label className="text-xl">Opis czynności wykonanych po ekspozycji</label>
+						<textarea
+							className="border p-2 rounded-2xl bg-white shadow-xs"
+							{...register("actionsAfterExposure", {
+								required: "Pole wymagane",
+								minLength: { value: 3, message: "Min. 3 znaki" },
+								maxLength: { value: 20, message: "Max. 20 znaków" },
+							})}
+						/>
+						{errors.actionsAfterExposure && (
+							<p className="text-red-500 text-sm">
+								{errors.actionsAfterExposure.message}
+							</p>
+						)}
+					</div>
 				</div>
 
-				<button className="cursor-pointer w-full border-2 py-2 rounded-2xl bg-blue-300 hover:bg-blue-500 text-xl ">Wyślij</button>
+				<button className="cursor-pointer w-full border-2 py-5 rounded-2xl hover:text-white hover:bg-blue-600 text-2xl border-blue-600 text-blue-600 mb-4 font-semibold mt-5 shadow-md ">
+					Wyślij
+				</button>
 			</form>
-
-     
 		</div>
 	);
 }
